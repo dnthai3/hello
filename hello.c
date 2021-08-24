f67a937d3bbe025a03bd9697283676ce35fc9c10
dnthai3 <thainguyenhaidang@huskers.unl.edu>
2021-08-24 20:32:02 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nguyen Hai Dang Thai
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main (int argc, char **argv) {
+
+     printf("Nguyen Hai Dang Thai!\n");
+
+     return 0;
+ }
