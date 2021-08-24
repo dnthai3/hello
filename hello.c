e9dae22e710af2b36bbe8779a1c1a7d584be25aa
Jeffrey Church <jefflchurch@huskers.unl.edu>
2021-08-24 21:14:08 +0000

    added collaborator name and major

--- hello.c
+++ hello.c
@@ -12,5 +12,8 @@
 
      printf("Nguyen Hai Dang Thai!\n");
      printf("My major: Actuarial Science\n");
+     printf("Jeffrey Church\n");
+     printf("My major: Computer Science\n"):
+     
      return 0;
  }
